9d91730bc5cf50ac224d328332d8646a32c158c1
muhammadyaumil1313 <muhammadyaumil1313@gmail.com>
2023-01-20 19:12:01 +0700

    initial commit

---
+++ src/main/kotlin/TipeDataNumber.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var age: Byte = 30
+    var value: Float = 29.0202F
+    var radius: Double = 30.0923
+    //we can use separator
+    var number: Int = 100_000_000
+    //conversion
+    var double : Double = number.toDouble()
+    println("from $number to $double")
+}
