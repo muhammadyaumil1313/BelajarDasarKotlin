3da1f4445fb59cc12e71b4d7d6d24f02377e99c2
muhammadyaumil1313 <muhammadyaumil1313@gmail.com>
2023-01-21 18:38:58 +0700

    ForLoop

---
+++ src/main/kotlin/ForLoops.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val names = arrayOf("EKo","Kurniawan","Friska","Audy","Bunga")
+    var total = 0
+    for (name in names){
+        println(name)
+        total++
+    }
+    println("Total Perulangan $total")
+
+    for(i in 0..100 step 2){
+        println(i)
+    }
+
+    for (j in 100 downTo 0 step 2){
+        println(j)
+    }
+
+    val ukuranArray = names.size - 1
+    for (i in 0..ukuranArray){
+        println("index $i array : ${names.get(i)}")
+    }
+}
