e2ff91578544b36cf699417d1e958f5c32ca9c3b
muhammadyaumil1313 <muhammadyaumil1313@gmail.com>
2023-01-29 13:34:40 +0700

    recursive

---
+++ src/main/kotlin/Recursive.kt
@@ -0,0 +1,16 @@
+fun factorialLoop(value: Int) : Int{
+    var result = 1
+    for(i in value downTo 1){
+        result *= i
+    }
+    return result
+}
+fun factorialRecursive(value : Int) : Int{
+    return when(value){
+        1->1
+        else->value * factorialRecursive(value - 1)
+    }
+}
+fun main() {
+    println(factorialRecursive(10))
+}
